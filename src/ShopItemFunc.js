451001cdffc3654ae283d048c0acced5b16ad939
lkamarova <lilija.kamarova@gmail.com>
2022-08-26 12:42:32 +0200

    initial commit

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ShopItemFunc extends Component {
+  render() {
+    return (
+      <div class="main-content">
+        <h2>{this.props.item.brand}</h2>
+        <h1>{this.props.item.title}</h1>
+        <h3>{this.props.item.description}</h3>
+        <div class="description">{this.props.item.descriptionFull}</div>
+        <div class="highlight-window mobile">
+          <div class="highlight-overlay"></div>
+        </div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{`${this.props.item.currency}${this.props.item.price}`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemFunc;
